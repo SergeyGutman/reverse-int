e5af9d048c60176eb28836b062225a92ac81b12c
Sergey <88570614+SergeyGutman@users.noreply.github.com>
2022-01-12 09:28:22 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let numToStr = String(n);
+  let lastIndex = numToStr.length - 1;
+  let result = '';
+  for (let i = lastIndex; i >= 0; i--){
+      result += numToStr[i];
+  }
+  return result;
 }
